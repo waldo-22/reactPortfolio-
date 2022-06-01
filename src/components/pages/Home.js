652ffc12b094a538fd12bf34ec57733954aca350
Reniel Christianson <waldochristianson@gmail.com>
2022-06-01 15:43:51 -0500

    files added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,19 +3,66 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Greetings! My name is Reniel Christianson</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      I am beginner full stack developer. I am a determined and driven individual that is looking to embark on this journey of coding. I have learned so much about JS, react, MySQL, CSS, HTML ect. There is always something new to learn or practice and get better at!
       </p>
+      <a href="https://github.com/waldo-22"><button>github</button></a>
+      <a href="https://www.linkedin.com/in/reniel-christianson-677575189"><button>LinkedIn</button></a>
+      <a href="https://docs.google.com/document/d/1liyCcq_EasESWtMTfsrcsYcqI-m5U1Nc/edit?usp=sharing&ouid=110244157607918638497&rtpof=true&sd=true"><button>Click to view Resume</button></a>
+      <div>
+      <h1>Skills And Trade</h1>
+      <p>These are a list of services I am able to provide and apply with the skills and knowledge I have gained:</p>
+
+      <h3>PWA: lorem********************</h3>
+      <h3>MERN: lorem********************</h3>
+      <h3>CSS: lorem********************</h3>
+      <h3>HTML: lorem********************</h3>
+      <h3>React: lorem********************</h3>
+      <h3>JavaScript: lorem********************</h3>
+      <h3>SQL: lorem********************</h3>
+      <h3>ExpressJS: lorem********************</h3>
+      <h3>NodeJS: lorem********************</h3>
+      <h3>API's (Server-side, and Third-party): lorem********************</h3>
+      <h3>No SQL: lorem********************</h3>
+
+      </div>
+      <div>
+      <h1>Projects/Work:</h1>
+      <p>These are some fully functional projects I have worked on and built.</p>
+
+      <h3>Furiously Fast "Fast & Furious" Facts!</h3>
+      <h1>Fetch API's</h1>
+      <h4>Description: With the use of weather map APIs, we are able to display a select amount information from a searched/selected city. We are also able predict and display 5 days ahead of time so the user can fully prepare and be knowledgeable of whats to come. </h4>
+
+      <h3>Weather Data</h3>
+      <h1>Fetch API's</h1>
+      <h4>Description: With the use of 3 APIs, we can provide users with information on the 9 movies in the Fast & Furious franchise. When users select a movie from the dropdown menu, they're presented with that movie's title, brief description, plot, and a related video. From Wikipedia's API, the title and brief are data we fetched. YouTube's API provides a video ID, which we inserted into a URL to embed that video to the page. Lastly, the movie's plot is sourced from OMDb's API. Using an audio tag, the song, 'Tokyo Drift' plays upon page load. Local storage is used to save the user's previous dropdown inquiry. </h4>
+
+      <h3>Password Generator</h3>
+      <h1>HTML, CSS, JavaScript </h1>
+      <h4>Description: Creates application that enables users to generate random passwords based on criteria that they’ve selected. Features dynamically updated HTML and CSS powered by JavaScript code. It is a clean and polished, responsive user interface that adapts to multiple screen sizes. </h4>
+
+      <h3>Tech Blog</h3>
+      <h1>Handlebars, SQL, ExpressJS</h1>
+      <h4>Description: CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. Follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. </h4>
+      </div>
+      
+
+      <div>Try My Games!</div>
+
+      <h1>Rock, Paper, Scissors</h1>
+      <h3>Description: Play rock, paper, or Scissors with an AI!</h3>
+      
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
